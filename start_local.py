8fa0e1177ac08cd8c0cc2a1a1146287a94ce85c9
Lx017 <92663419+Lx017@users.noreply.github.com>
2023-12-20 17:42:34 -0500

    lx fix

--- start_local.py
+++ start_local.py
@@ -1,9 +1,28 @@
 from XComClient import ComClient
+import socket
+
+def get_local_ip():
+    try:
+        # Create a socket object
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # No need to connect to a specific server
+        # Using Google's public DNS server address and an arbitrary port number
+        s.connect(("8.8.8.8", 80))
+        # Get the IP address of the socket's local endpoint
+        ip = s.getsockname()[0]
+        # Close the socket
+        s.close()
+        return ip
+    
+    except Exception as e:
+        return f"Error occurred: {e}"
 
 
 if __name__ == "__main__":
+    local_ip = get_local_ip()
+    print(f"Local IP Address: {local_ip}")
     comclient = ComClient()
     comclient.registerFunc("image2image")
-    comclient.asServer("192.168.178.45", 8765)
+    comclient.asServer(local_ip, 8765)
 
 
